ea09e1ab0ddc6882a5fd2f227639da604a9869ba
zbigniewcichanski <zbigniewcichanski@gmail.com>
2015-09-18 23:24:26 +0200

    commit help

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
- 
+
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -20,7 +20,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
  * Controller used to manage the application security.
  * See http://symfony.com/doc/current/cookbook/security/form_login_setup.html.
  *
- * @author Ryan Weaver <weaverryan@gmail.com>
+ * @author Zbigniew Cichanski <weaverryan@gmail.com>
  * @author Javier Eguiluz <javier.eguiluz@gmail.com>
  */
 class SecurityController extends Controller
